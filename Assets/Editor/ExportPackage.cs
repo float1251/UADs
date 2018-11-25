ad8fd289416b2c40903c0a6bda156f906fe2de82
float1251 <taka.red.0502@gmail.com>
2018-11-25 10:18:41 +0900

    v0.1.0をリリース.

--- Assets/Editor/ExportPackage.cs
+++ Assets/Editor/ExportPackage.cs
@@ -7,9 +7,25 @@ public class ExportPackage : ScriptableObject
 	static void DoIt()
 	{
 		var txt = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/UAds/VERSION.txt");
-		// TODO: export前に初期設定にする
+		var setting = AssetDatabase.LoadAssetAtPath<UAds.UAdsSetting>("Assets/UAds/UAdsSettings.asset");
+		// データ初期化前に設定保持しておき、終了したら復元する
+		var original = EditorJsonUtility.ToJson(setting);
+
+		//  export前に初期設定にする
+#if UNITY_ADS || UNITY_MONETIZATION
+		setting.unityAds.androidGameId = "";
+		setting.unityAds.iOSGameId = "";
+#endif
+
+#if ENABLE_ADCOLONY
+		setting.adColony.androidSetting = new UAds.UAdsSetting.AdColoySetting.Setting();
+		setting.adColony.iOSSetting = new UAds.UAdsSetting.AdColoySetting.Setting();
+#endif
+
+		UAds.Editor.UAdsSettingHelper.SaveUAdsSettings(setting);
+
 		EditorUtility.DisplayProgressBar("Export Package", "Processing...", 0.1f);
-		var packageName = string.Format("uads_{0}.unitypackage", txt.text.Trim());
+		var packageName = "uads.unitypackage";
 		try {
 
 			AssetDatabase.ExportPackage("Assets/UAds", packageName, ExportPackageOptions.Recurse);
@@ -17,7 +33,10 @@ public class ExportPackage : ScriptableObject
 			Debug.Log("create " + packageName);
 		}
 		finally {
+			// 復元処理
 			EditorUtility.ClearProgressBar();
+			EditorJsonUtility.FromJsonOverwrite(original, setting);
+			UAds.Editor.UAdsSettingHelper.SaveUAdsSettings(setting);
 		}
 	}
 }
